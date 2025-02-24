31d6377139b1723dc0c78de4280d45a280c2c1fb
김서영 <pepper2727@mediazen.co.kr>
2025-02-24 10:14:29 +0900

    Revert "buffer 와 stream 성능 비교_250224"
    This reverts commit a4237bd7129e638b8515ef7d9134b28c82545b47.

--- ch3/buffer-stream/compare-2.js
+++ ch3/buffer-stream/compare-2.js
@@ -3,19 +3,11 @@
 
 const fs = require('fs');
 
-// 작업 시작 전 메모리 사용량 (약 28.57 MB)
+// 작업 시작 전 메모리 사용량
 console.log(`before: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`);
 
 const data1 = fs.readFileSync('./compare.txt');
 fs.writeFileSync('./compare2.txt', data1);
 
-// 작업 직후 메모리 사용량 (약 130.41 MB)
+// 작업 직후 메모리 사용량
 console.log(`buffer: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`);
-// ** 이때 작업이 끝났음에도 메모리 사용량이 높게 측정되는 이유
-// : 작업이 끝난 즉시 사용된 메모리가 해제되지 않고, 메모리 해제하는 데 시간이 필요하기 때문.
-
-// 메모리 해제 시간 확인
-// 현재 테스트 시 5-10ms 정도 소요
-setTimeout(function() {
-    console.log(`done: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`);
-}, 5);
--- ch3/buffer-stream/compare-3.js
+++ ch3/buffer-stream/compare-3.js
@@ -3,14 +3,11 @@
 
 const fs = require('fs');
 
-// 작업 시작 전 메모리 사용량 (약 28.52 MB)
 console.log(`before: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`);
 
 const readStream = fs.createReadStream('./compare.txt');
 const writeStream = fs.createWriteStream('./compare3.txt');
 readStream.pipe(writeStream);
-
-// 작업 직후 메모리 사용량 (약 45.00 MB)
 readStream.on('end', () => {
     console.log(`stream: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`);
 });
